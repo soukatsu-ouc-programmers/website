556253f6a8bcd8f7b3a8530c396c6f853fc965da
himantyu-yuma <lipton.happy.milktea@gmail.com>
2021-07-21 23:24:01 +0900

    #5 作品投稿ページでもパブリサイズ共有が有効になるように修正
    ついでにカスタム投稿タイプでもクラシックエディター拡張機能を無効にしたときにブロックエディタに変更されるようにも修正

--- theme/custom-post-type.php
+++ theme/custom-post-type.php
@@ -24,7 +24,8 @@ function new_post_type_works() {
                 'custom-fields',       // カスタムフィールド
                 'comments'             // コメント
             ),
-            'menu_position' => 5       // 「投稿」の下に追加
+            'menu_position' => 5,      // 「投稿」の下に追加
+            'show_in_rest' => true
         )
     );
 
@@ -91,7 +92,8 @@ function new_post_type_leonardotoys() {
                 'editor',              // 本文（の編集機能）
                 'custom-fields',       // カスタムフィールド
             ),
-            'menu_position' => 5       // 「投稿」の下に追加
+            'menu_position' => 5,      // 「投稿」の下に追加
+            'show_in_rest' => true
         )
     );
 
